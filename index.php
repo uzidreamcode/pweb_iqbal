c3d3434e33c3489d889b408fbf5e99f779612df2
ukanaadamm <77190656+uzidreamcode@users.noreply.github.com>
2021-11-07 16:58:09 +0700

    fix index

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Purple Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- End layout styles -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico" />
+</head>
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth">
+        <div class="row flex-grow">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left p-5">
+              <div class="brand-logo">
+                <img src="assets/images/logo.svg">`
+              </div>
+              <h4>Hello! let's get started</h4>
+              <h6 class="font-weight-light">Sign in to continue.</h6>
+              <form method="post" class="pt-3">
+                <div class="form-group">
+                  <input type="text" name="username" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                </div>
+                <div class="form-group">
+                  <input type="password" name="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="mt-3">
+                  <button name="submit" class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" >Login</button>
+                </div> 
+                
+              </form>
+              <?php
+                          include 'koneksi.php';  
+                          if (isset($_POST['submit'])) 
+                          {
+                            $user= $_POST["username"];
+                            $pass= md5($_POST['password']); 
+                            $ambil=$koneksi->query("SELECT * FROM spp WHERE username='$user' AND password='$pass'");
+                                    $pecah = $ambil->fetch_assoc();
+                                    $benar =$ambil->num_rows;
+                            if ($benar > 0) 
+                                    {   
+                              echo "<script>alert ('Password benar')</script>"; 
+                                         $_SESSION['iduser'] =$user;
+                                        $_SESSION['fullname'] = $user;
+                                        $_SESSION['admin'] = $pecah['admin'];
+                                       header("Location: admin.php");
+                                          die();
+
+                            }
+                            else
+                            {
+                              echo "<script>alert ('Password salah')</script>"; 
+                            }
+                          }
+                          ?>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../../assets/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="../../assets/js/off-canvas.js"></script>
+  <script src="../../assets/js/hoverable-collapse.js"></script>
+  <script src="../../assets/js/misc.js"></script>
+  <!-- endinject -->
+</body>
+</html>
