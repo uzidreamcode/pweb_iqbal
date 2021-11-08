ae084b0683fafe45d397640a2d52579dde79ea26
ukanaadamm <77190656+uzidreamcode@users.noreply.github.com>
2021-11-08 19:32:22 +0700

    halaman anak

---
+++ home.php
@@ -0,0 +1,7 @@
+<div class="container">
+              <div class="jumbotron">
+              <h2 align="center">APLIKASI PEMBAYARAN SPP SMKN 1 KEPANJEN</h2>
+              <p align="center">Jl. Raya Kedungpedaringan Kepanjen</p>
+              <p align="center">Selamat Datang<strong> <?php echo $_SESSION['fullname']; ?></strong></p>
+            </div>
+            </div>
